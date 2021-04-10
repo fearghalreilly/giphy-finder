cfd0636a68281f30d73b1501e13d82a9b0e1b7ef
fearghalreilly <74102899+fearghalreilly@users.noreply.github.com>
2021-04-10 16:32:44 +0100

    Nav component created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-
+import GlobalStyles from './components/GlobalStyles'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+
+//Components
+import Nav from './components/Nav'
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <BrowserRouter>
+    <GlobalStyles />
+    <Nav />
+    <div>
       <h1>GIPHY FINDER</h1>
      
     </div>
+    </BrowserRouter>
+    </>
   );
 }
 
