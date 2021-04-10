a25461548c88b1f63b6842f87a77c28fd75d1554
fearghalreilly <74102899+fearghalreilly@users.noreply.github.com>
2021-04-10 18:41:49 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, {useEffect} from 'react'
+import axios from 'axios'
 import GlobalStyles from './components/GlobalStyles'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
@@ -5,17 +7,25 @@ import './App.css';
 //Components
 import Nav from './components/Nav'
 
+//State
+import GiphyState from './context/giphy/GiphyState'
+
 function App() {
+
+
   return (
     <>
+    <GiphyState>
     <BrowserRouter>
     <GlobalStyles />
     <Nav />
     <div>
       <h1>GIPHY FINDER</h1>
+   
      
     </div>
     </BrowserRouter>
+    </GiphyState>
     </>
   );
 }
