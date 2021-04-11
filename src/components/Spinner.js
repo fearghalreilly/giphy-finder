e1ae3cfca4d5841337fe40c826df6bb992bf4c5d
fearghalreilly <74102899+fearghalreilly@users.noreply.github.com>
2021-04-11 09:14:55 +0100

    Loading Gif Spinner Component

---
+++ src/components/Spinner.js
@@ -0,0 +1,10 @@
+import React  from 'react'
+import spinner from '../images/spinner.gif';
+
+const Spinner = () => 
+
+    <>
+      <img src={spinner} alt="Loading..." style={{ width: '200px', margin: 'auto', display: 'block' }} />
+    </>
+
+export default Spinner
